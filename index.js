48fd42aa86e1d69f5dab40d0c2cbdda83abfd371
Leo <115810918+Lev156@users.noreply.github.com>
2023-06-21 07:25:44 +0300

    Update index.js
    Это изменение частично изменила логику подсчёта общий суммы и исправило проблемы
    + вычитает и удаляет карточку из общеё суммы при нажатие  "-"
    + исправлено подсчитывание в общую сумму

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-let price = 0;
-let price_1 = 0;
-let price_2 = 0;
-let price_3 = 0;
-let price_4 = 0;
-let price_5 = 0;
-let price_6 = 0;
-let price_7 = 0;
-let price_8 = 0;
-let price_9 = 0;
-let price_10 = 0;
+let a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 // Раздел по добавлению и удалению строк
 let cont = document.querySelector(`.cont`);
 let r = 0;
@@ -24,10 +15,7 @@ let cart_8 = document.querySelector(`#cart_8`);
 let cart_9 = document.querySelector(`#cart_9`);
 let cart_10 = document.querySelector(`#cart_10`);
 
-
-
-document.querySelector(`#add_плюс`).addEventListener(`click`, function (evt) {
-    
+document.querySelector(`#add_плюс`).addEventListener(`click`, function (evt) {    
     if (r == 0) {
         cart_1.classList.remove("d-none");
     } else if (r == 1) {
@@ -48,101 +36,99 @@ document.querySelector(`#add_плюс`).addEventListener(`click`, function (evt)
         cart_9.classList.remove("d-none");
     } else if (r == 10) {
         cart_10.classList.remove("d-none");
-    }
+    };
 
     if (r >= 0 && r <= 9){
         r++;
-    }
-    console.log(r)
+    };
 });
-document.querySelector(`#add_минус`).addEventListener(`click`, function (evt) {
-    
+
+document.querySelector(`#add_минус`).addEventListener(`click`, function (evt) {    
     if (r == 1) {
         cart_1.classList.add("d-none");
+        a[1] = 0;
     } else if (r == 2) {
         cart_2.classList.add("d-none");
+        a[2] = 0;
     } else if (r == 3) {
         cart_3.classList.add("d-none");
+        a[3] = 0;
     } else if (r == 4) {
         cart_4.classList.add("d-none");
+        a[4] = 0;
     } else if (r == 5) {
         cart_5.classList.add("d-none");
+        a[5] = 0;
     } else if (r == 6) {
         cart_6.classList.add("d-none");
+        a[6] = 0;
     } else if (r == 7) {
         cart_7.classList.add("d-none");
+        a[7] = 0;
     } else if (r == 8) {
         cart_8.classList.add("d-none");
+        a[8] = 0;
     } else if (r == 9) {
         cart_9.classList.add("d-none");
+        a[9] = 0;
     } else if (r == 10) {
         cart_10.classList.add("d-none");
-    }
+        a[10] = 0;
+    };
+
     if (r >= 1 && r <= 11){
         r--;
-    }
-    console.log(r)
+    };
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`;
 });
 
 
 
-
-
-
-// // Раздел по подщёт общей сумы
+// Раздел по подщёт общей сумы
 let th_2 = document.querySelector(`.th-2`);
 
 
 document.querySelector(`#price`).addEventListener(`input`, function (evt) {
-    price = Number(evt.target.value);
-    th_2.innerHTML = `${price}₽`
+    a[0] = Number(evt.target.value);
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`; 
 });
-
 document.querySelector(`#price_1`).addEventListener(`input`, function (evt) {
-     price_1 = Number(evt.target.value);
-    th_2.innerHTML = `${price + price_1}₽`
+    a[1] = Number(evt.target.value);
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`;
 });
 document.querySelector(`#price_2`).addEventListener(`input`, function (evt) {
-     price_2 = Number(evt.target.value);
-    th_2.innerHTML = `${price + price_1 + price_2}₽`
+    a[2] = Number(evt.target.value);
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`;
 });
 document.querySelector(`#price_3`).addEventListener(`input`, function (evt) {
-     price_3 = Number(evt.target.value);
-    th_2.innerHTML = `${price + price_1 + price_2 + price_3}₽`
+    a[3] = Number(evt.target.value);
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`;
 });
 document.querySelector(`#price_4`).addEventListener(`input`, function (evt) {
-     price_4 = Number(evt.target.value);
-    th_2.innerHTML = `${price + price_1 + price_2 + price_3 + price_4}₽`
+    a[4] = Number(evt.target.value);
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`;
 });
 document.querySelector(`#price_5`).addEventListener(`input`, function (evt) {
-     price_5 = Number(evt.target.value);
-    th_2.innerHTML = `${price + price_1 + price_2 + price_3 + price_4 + price_5}₽`
+    a[5] = Number(evt.target.value);
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`;
 });
 document.querySelector(`#price_6`).addEventListener(`input`, function (evt) {
-     price_6 = Number(evt.target.value);
-    th_2.innerHTML = `${price + price_1 + price_2 + price_3 + price_4 + price_5 + price_6}₽`
+    a[6] = Number(evt.target.value);
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`;
 });
 document.querySelector(`#price_7`).addEventListener(`input`, function (evt) {
-     price_7 = Number(evt.target.value);
-    th_2.innerHTML = `${price + price_1 + price_2 + price_3 + price_4 + price_5 + price_6 + price_7}₽`
+    a[7] = Number(evt.target.value);
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`;
 });
 document.querySelector(`#price_8`).addEventListener(`input`, function (evt) {
-     price_8 = Number(evt.target.value);
-    th_2.innerHTML = `${price + price_1 + price_2 + price_3 + price_4 + price_5 + price_6 + price_7 + price_8}₽`
+    a[8] = Number(evt.target.value);
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`;
 });
 document.querySelector(`#price_9`).addEventListener(`input`, function (evt) {
-     price_9 = Number(evt.target.value);
-    th_2.innerHTML = `${price + price_1 + price_2 + price_3 + price_4 + price_5 + price_6 + price_7 + price_8 + price_9}₽`
+    a[9] = Number(evt.target.value);
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`;
 });
 document.querySelector(`#price_10`).addEventListener(`input`, function (evt) {
-     price_10 = Number(evt.target.value);
-    th_2.innerHTML = `${price + price_1 + price_2 + price_3 + price_4 + price_5 + price_6 + price_7 + price_8 + price_9 + price_10}₽`
+    a[10] = Number(evt.target.value); 
+    th_2.innerHTML = `${a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10]}₽`;
 });
-
-
-
-
-
-
-
-
